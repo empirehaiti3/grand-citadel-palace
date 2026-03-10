c54b2b6506c09a353bb72d21968f4e567c3d903b
empirehaiti3 <empirehaiti3@gmail.com>
2026-03-10 07:42:59 +0000

    SOVEREIGN ASCENSION

--- app/page.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function Home() {
-return (
-<div className="flex flex-col items-center min-h-screen bg-black text-white font-sans overflow-x-hidden">
-
-);
-}
