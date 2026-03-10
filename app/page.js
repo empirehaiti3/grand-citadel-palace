30d6b5e5e3708a9aef391f2624e6ec91d484cf57
empirehaiti3 <empirehaiti3@gmail.com>
2026-03-10 07:09:58 +0000

    PALACE COMMAND 2026

---
+++ app/page.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Home() {
+return (
+<div className="flex flex-col items-center min-h-screen bg-black text-white font-sans overflow-x-hidden">
+
+);
+}
