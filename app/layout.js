8b77337c40b1baac37a316f81bb39db3cb622ad8
empirehaiti3 <empirehaiti3@gmail.com>
2026-03-25 13:19:53 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,10 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'Grand Citadel Palace',
+  description: 'GCS Gateway',
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
