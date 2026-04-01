140d45a3b07c66c71b1d71a5b1a11d2cbce8ddb1
empirehaiti3 <empirehaiti3@gmail.com>
2026-04-01 16:19:33 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,19 @@
-<script src="/zoe.js" defer></script>
+import "./globals.css";
+
+export const metadata = {
+  title: "Grand Citadel Palace",
+  description: "The Sovereign Seat of the House of Dorvilus",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <script src="/zoe.js" defer></script>
+      </head>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
